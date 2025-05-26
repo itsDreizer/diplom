dfdf2543c84248758a426d8fdeca176d2c990469
itsDreizer <artem39r@mail.ru>
2025-05-26 03:07:49 +0200

    ...

--- sendData_protected.php
+++ sendData_protected.php
@@ -2,7 +2,7 @@
 include "connect.php";
 
 $textData = $_POST["textData"];
-// Экранируем HTML-сущности, чтобы предотвратить XSS
+
 $textData = htmlspecialchars($textData, ENT_QUOTES, 'UTF-8');
 
 $stmt = $db->prepare("INSERT INTO `comments` (`textdata`) VALUES (?)");
